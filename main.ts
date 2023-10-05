a540afafe615286540219fec0e620cc0f0b1f872
angelo-yalung <144264086+angelo-yalung@users.noreply.github.com>
2023-10-05 13:26:28 -0400

    f

--- main.ts
+++ main.ts
@@ -12,18 +12,18 @@ basic.pause(1000)
 // colors
 input.onButtonPressed(Button.A, function() {
   pins.digitalWritePin(DigitalPin.P14, 1)
-  basic.showString("Red")
+  basic.showString("Green") //BLue
 
   basic.pause(1000)
 
   pins.digitalWritePin(DigitalPin.P14, 0)
-  basic.showString("Blue")
+  basic.showString("Blue") 
   pins.digitalWritePin(DigitalPin.P15,1)
 
   basic.pause(1000)
 
   pins.digitalWritePin(DigitalPin.P15, 0)
-  basic.showString("Green")
+  basic.showString("Red") 
   pins.digitalWritePin(DigitalPin.P16, 1)
 
   basic.pause(1000)
@@ -47,6 +47,12 @@ input.onButtonPressed(Button.A, function() {
 
   pins.digitalWritePin(DigitalPin.P15,1)
   pins.digitalWritePin(DigitalPin.P14,0)
-})
 
-basic.clearScreen()
+  basic.pause(1000)
+
+  pins.digitalWritePin(DigitalPin.P14,0)
+  pins.digitalWritePin(DigitalPin.P15,0)
+  pins.digitalWritePin(DigitalPin.P16,0)
+
+  basic.clearScreen()
+})
